9e9278c091d3168dec8a28a9b5975dabc985afeb
m-marquez-medina21 <m.marquez.medina21@gmail.com>
2021-05-06 23:37:03 -0400

    add:style - script

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+$(function(){
+
+    $("a").click(function(event){
+        if(this.hash !== ""){
+            event.preventDefault();
+
+            let gato = this.hash;
+
+            $("html, body").animate({
+                scrollTop: $(gato).offset().top
+            }, 800, function(){
+                window.location.hash=gato;
+            });
+        }
+    });
+});
